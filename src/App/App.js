cb6f770127daf9654d2c7c82f1e19be2f9120f61
Emily DeWitt <emilykdewitt@gmail.com>
2019-06-18 20:41:44 -0500

    Finished setup files

---
+++ src/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button className='btn btn-danger'>HELP ME</button>
+    </div>
+  );
+}
+
+export default App;
